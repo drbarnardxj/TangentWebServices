d1922820d01b7683f6fbe9451e8e3de680dff734
Deon Barnard <deon@dcbmedia.co.za>
2018-02-27 08:58:47 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+(function () {  
+    'use strict';  
+  
+    var app = angular.module("mainApp", ['ui.router']);  
+  
+    app.constant("appSetting", {  
+        apiBaseUrl: "/"  
+    });  
+  
