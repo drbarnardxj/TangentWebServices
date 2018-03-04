a3c9d89ea61e2a12760076e3b187a0c70aca15de
drbarnardxj <deon@dcbmedia.co.za>
2018-03-04 18:15:15 +0200

    Collecting data for dashboard

--- App/js/controllers/dashController.js
+++ App/js/controllers/dashController.js
@@ -1,21 +1,57 @@
 app.controller('dashController', ['$scope', '$routeParams', '$http',function($scope, $routeParams, $http) {
-  
-  
-		$(".loadingScreen").fadeIn(300);	
-		$http.get('http://staging.tangent.tngnt.co/api/employee/', {headers: { Authorization: ' Token ' + sessionStorage.getItem('accessToken')}})
+
+	$(".loadingScreen").fadeIn(300);
+	$http.get('http://staging.tangent.tngnt.co/api/employee/me/', {headers: { Authorization: ' Token ' + sessionStorage.getItem('accessToken')}})
          .success(function(data) {
-           $scope.dash = data;
-           $scope.dash.position = {}
-           for (var key in $scope.dash){
-           	
-           }
+         	$scope.reviewDate = data.next_review;
+         	$scope.birthDate = data.birth_date;
          })
-         .error(function(data) {
+	
+	
+	
+	$http.get('http://staging.tangent.tngnt.co/api/employee/', {headers: { Authorization: ' Token ' + sessionStorage.getItem('accessToken')}})
+        .success(function(data) {
+        	var oneDay = 24*60*60*1000;
+        	var d = new Date();
+			var month = d.getMonth() + 1; // set current month to a usable int for later
+			var reviewDate = $scope.reviewDate.split("-");
+			var review = new Date(parseInt(reviewDate[0]),parseInt(reviewDate[1])-1,parseInt(reviewDate[2]));
+			$scope.daysToReview = Math.round(Math.abs((d.getTime() - review.getTime())/(oneDay)));
+			var birthDate = $scope.birthDate.split("-");
+			var bDay = new Date(d.getFullYear(),parseInt(birthDate[1]),parseInt(birthDate[2]));
+			$scope.daysToBirthDay = Math.round(Math.abs((d.getTime() - bDay.getTime())/(oneDay)));
+			
+			console.log("current year: " +d.getFullYear('YYYY'));
+        	$scope.dash = data;
+        	$scope.dashData = {staffTotal : 0, birthdaysTotals: 0, maleTotals : 0, femaleTotals : 0}
+        	$scope.staffTotal = 0;
+        	$scope.birthdaysTotals = 0;
+        	$scope.maleTotals = 0;
+        	$scope.femaleTotals = 0;
+        	$scope.birthdayRecord = [];
+        	$scope.positions = [];
+            console.log("review: " + $scope.reviewDate);
+        	for (var key in $scope.dash){
+           		$scope.dashData.staffTotal++;
+           		try {
+           			$scope.positions[$scope.dash[key].position.name]++;
+           		} catch(e) {
+           			$scope.positions.push({name: $scope.dash[key].position.name, count : 1})
+           		}
+           		console.log($scope.dash[key].position.name);
+           		if ($scope.dash[key].gender === "M"){ $scope.dashData.maleTotals++; } else { $scope.dashData.femaleTotals++; }
+           		if (parseInt($scope.dash[key].birth_date.substr(5,2)) === month) {
+           			$scope.dashData.birthdaysTotals++;
+           			$scope.birthdayRecord.push($scope.dash[key])
+           		} else {} // get birthdays for current month
+        	}
+        })
+        .error(function(data) {
          	console.log("Could not retrieve data from: http://staging.tangent.tngnt.co/api/employee/ with Token : " + sessionStorage.getItem('accessToken') );
          	console.log(JSON.stringify(data));
            
-         });
-		$(".loadingScreen").fadeOut(300);
+        });
+	$(".loadingScreen").fadeOut(300);
 
   
 }]);
