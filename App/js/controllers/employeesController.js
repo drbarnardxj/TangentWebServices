674a29ebfd8dd6e7506027a569b06cd86865905f
drbarnardxj <deon@dcbmedia.co.za>
2018-03-04 22:52:40 +0200

    Employees list ready for extra data

--- App/js/controllers/employeesController.js
+++ App/js/controllers/employeesController.js
@@ -1,24 +1,24 @@
 app.controller('employeesController', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
 	// fill data while fetching from API via myProfile
-	$scope.employee = {
-		"id": "fetching..."
-	};
+
 	$scope.filter = {};
 	$scope.filter.race = "";
 	$scope.filter.position = "";
 	$scope.filter.gender = "";
+	$scope.filter.email__contains = "";
 	var filterString = "";
 	$scope.searchFilter = function(){
 		$(".loadingScreen").fadeIn(300);
 		filterString = "?";
 		var i = 0;
+		$scope.filter.email__contains = $scope.filter.email__contains.toLowerCase();
 		for (var key in $scope.filter) {
 			i++;
 			console.log(key + ": " + $scope.filter[key] + "Active Filters: " + Object.keys($scope.filter).length);
 			filterString += key + "=" + $scope.filter[key];
 			if (i !== Object.keys($scope.filter).length) {filterString += "&";}
 		}
-		console.log("Search button clicked with data :" + $scope.filter.race);
+		
 		$http.get('http://staging.tangent.tngnt.co/api/employee/' + filterString, {headers: { Authorization: ' Token ' + sessionStorage.getItem('accessToken')}})
          .success(function(data) {
            $scope.employee = data;
@@ -32,6 +32,5 @@ app.controller('employeesController', ['$scope', '$routeParams', '$http', functi
 	}
 	
 	$scope.searchFilter();
-
 	
 }]);
