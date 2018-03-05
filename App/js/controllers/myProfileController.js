8a796bb96f56c211f723ae28c7cd24a31d68f7cc
drbarnardxj <deon@dcbmedia.co.za>
2018-03-05 09:29:55 +0200

    Indentation correction

--- App/js/controllers/myProfileController.js
+++ App/js/controllers/myProfileController.js
@@ -1,13 +1,8 @@
 app.controller('myProfileController', ['$scope', 'myProfile', '$routeParams', function($scope, myProfile, $routeParams) {
-	// fill data while fetching from API via myProfile
 	$scope.detail = {
 		"id": "fetching..."
 	};
-//	$(".loadingScreen").fadeIn(300);
 	myProfile.success(function(data) {
-    	if (data.is_active) {data.is_active = "Yes";} else {data.is_active = "No";}
-    	if (data.is_staff) {data.is_staff = "Yes";} else {data.is_staff = "No";}
     	$scope.detail = data;
-    //	$(".loadingScreen").fadeOut(300);
 	});
 }]);
